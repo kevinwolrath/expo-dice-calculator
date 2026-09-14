beab4abed12ab46ebde0971d9bed91457f30340e
Kevin Wolrath <kevin.wolrath@gmail.com>
2026-09-14 12:03:55 +1000

    removing unused theme assets

--- assets/themes/tavern/assets.ts
+++ assets/themes/tavern/assets.ts
@@ -1,19 +1,6 @@
 const packAssets = {
   background: require("./tavern_background.jpg"),
-  backgroundDark: require("./tavern_background_dark.jpg"),
   banner: require("./dice_job_manager_banner.jpg"),
-  icon: require("./app_icon.png"),
-  splash: require("./splash_screen.jpg"),
-  panel: require("./panel_background.png"),
-  buttonPrimary: require("./button_primary.png"),
-  buttonWood: require("./button_wood.png"),
-  buttonOutline: require("./button_outline.png"),
-  dice: {
-    d20: require("./dice_d20.png"),
-    d12: require("./dice_d12.png"),
-    d8: require("./dice_d8.png"),
-    d6: require("./dice_d6.png"),
-  },
   icons: {
     notes: require("./icon_notes.png"),
     material: require("./icon_material.png"),
